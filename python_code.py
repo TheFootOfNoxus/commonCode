c81781c6646851490f951a5dc176fee40ac7d785
yj <yj@wanplus.com>
2018-11-26 17:02:20 +0800

    获取指定日期的时间戳

--- python_code.py
+++ python_code.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# 读取多个json文件的内容
 def read_json_file():
+    """
+    读取多个json文件的内容
+    """
     filelist = ['title.json', 'body.json']
     for file in filelist:
         with open(file, 'r') as f:
@@ -27,3 +29,24 @@ time_range = ['0.json', '1.json', '2.json']
 [all_file.append(time_prefix + num) for num in time_range]
 print(all_file)
 # reesult ['day_0.json', 'day_1.json', 'day_2.json']
+
+def __listdir(slef, path):
+    """
+    获取指定文件夹下的所有文件
+    """
+    list_name = list()
+    for file in os.listdir(path):
+        file_path = os.path.join(path, file)
+        if os.path.isdir(file_path):
+            pass
+        else:
+            list_name.append(file_path)
+    return list_name
+
+def get_time_stamp(days):
+    """
+    获取指定日期的时间戳:例如 获取7天前0的时间戳
+    """
+    day = datetime.datetime.today().date() - datetime.timedelta(days=7)
+    ts = int(time.mktime(time.strptime(str(day), '%Y-%m-%d')))
+    return ts
