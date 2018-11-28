a73a18be294cc7d9da5de04294e87b8b87da54c3
yj <yj@wanplus.com>
2018-11-28 10:42:17 +0800

    检查字符串中是否包含中文

--- python_code.py
+++ python_code.py
@@ -50,3 +50,58 @@ def get_time_stamp(days):
     day = datetime.datetime.today().date() - datetime.timedelta(days=7)
     ts = int(time.mktime(time.strptime(str(day), '%Y-%m-%d')))
     return ts
+
+def print_memory_info():
+    """
+    打印已占用内存数
+    :return:
+    """
+    import psutil
+
+    info = psutil.virtual_memory()
+    print u'内存使用：', psutil.Process(os.getpid()).memory_info().rss // 1024 #单位为Kb
+    print u'总内存：', info.total // 1048576 #单位为Mb
+    print u'内存占比：', info.percent
+    print u'cpu个数：', psutil.cpu_count()
+
+def today_timestamp():
+    """
+    当日0点时间戳
+    :return:
+    """
+    now = datetime.datetime.utcfromtimestamp(now_timestamp - now_timestamp % 86400)
+    return now
+
+def last_week_timestamp():
+    """
+    获取上周时间戳
+    :return:
+    """
+    last_week_start = int(time.mktime((now - datetime.timedelta(days=now.weekday() + 7)).timetuple()))
+    last_week_end = int(time.mktime((now - datetime.timedelta(days=now.weekday() + 1)).timetuple()))
+
+    return {'last_week_start': last_week_start, 'last_week_end': last_week_end}
+
+def json_encode_cn():
+    """
+    json格式化中文
+    :return:
+    """
+    f = open('../data/click/data.json', 'w')
+    json_list = list()
+    for item in items:
+        word_list = utils.analyze(item['content'])
+        if word_list:
+            json_list += word_list
+    f.write(json.dumps(json_list, ensure_ascii=False))
+    f.close()
+
+def check_contain_chinese(check_str):
+    """
+    检查字符串中是否包含中文
+    :return:
+    """
+    for ch in check_str.decode('utf-8'):
+        if u'\u4e00' <= ch <= u'\u9fff':
+            return True
+    return False
